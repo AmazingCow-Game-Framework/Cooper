d1ec7e205750f034b63474f9d21f1d491a067290
n2omatt <n2omatt@amazingcow.com>
2018-03-19 20:12:32 -0300

    - Add a private logger.

---
+++ Cooper/include/private/Logger/Logger.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// AmazingCow Libs
+#include "CoreLog/CoreLog.h"
+
+namespace Cooper { namespace Private {
+
+CoreLog::Logger* Logger() noexcept;
+
+} // namespace Private
+} // namespace Cooper
